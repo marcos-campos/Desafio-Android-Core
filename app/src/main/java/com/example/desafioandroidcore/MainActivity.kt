a5863c4c7721fd2ad07cb1ca4e581fa4a273aa5a
Marcos Campos <mvocampos@yahoo.com.br>
2021-03-11 14:41:20 -0300

    quarto commit

--- app/src/main/java/com/example/desafioandroidcore/MainActivity.kt
+++ app/src/main/java/com/example/desafioandroidcore/MainActivity.kt
@@ -4,18 +4,76 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import java.util.regex.Matcher
+import java.util.regex.Pattern
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val etUser = findViewById<EditText>(R.id.et_user)
+        val etPass = findViewById<EditText>(R.id.et_password)
+
         val btnLogin = findViewById<Button>(R.id.btn_login)
 
         btnLogin.setOnClickListener {
-            val intent = Intent(this, ListaRestauranteActivity::class.java)
-            startActivity(intent)
+
+
+            if (validacao(etUser.text.toString(), etPass.text.toString())){
+
+                val intent = Intent(this, ListaRestauranteActivity::class.java)
+                startActivity(intent)
+
+            } else {
+
+            Toast.makeText(this, "A senha tem que ter letra maiuscula, minuscula, número e um caracter especial", Toast.LENGTH_LONG).show()
         }
+
+        }
+    }
+
+
+    private fun validacao(nome: String, senha: String) : Boolean {
+
+        val userError = findViewById<EditText>(R.id.et_user)
+        val passError = findViewById<EditText>(R.id.et_password)
+
+        when {
+            nome.isEmpty() -> {
+                Toast.makeText(this, "Nome invalido", Toast.LENGTH_SHORT).show()
+                userError.error = "Campo obrigatório"
+
+                return false
+
+            }
+            senha.isEmpty() -> {
+                Toast.makeText(this, "Senha invalida", Toast.LENGTH_SHORT).show()
+                passError.error = "Campo obrigatório"
+                return false
+
+            }
+            else -> {
+
+                return passwordValidation(senha)
+
+            }
+        }
+
+    }
+
+    private fun passwordValidation(password: String): Boolean {
+        return if (password.length > 1) {
+            val letter: Pattern = Pattern.compile("[a-zA-z]")
+            val digit: Pattern = Pattern.compile("[0-9]")
+            val special: Pattern = Pattern.compile("[!@#$%&*()_+=|<>?{}\\[\\]~-]")
+            val hasLetter: Matcher = letter.matcher(password)
+            val hasDigit: Matcher = digit.matcher(password)
+            val hasSpecial: Matcher = special.matcher(password)
+            hasLetter.find() && hasDigit.find() && hasSpecial.find()
+        } else false
     }
 
 }
